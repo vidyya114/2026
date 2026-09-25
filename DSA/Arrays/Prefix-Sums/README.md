e182ddba72b34a4485e0ba62f1cb88942636474f
Vidya <vidyavidya7143@gmail.com>
2026-09-25 21:40:37 +0530

    Restructure: Move all array patterns into DSA/Arrays and add Prefix Sums

---
+++ DSA/Arrays/Prefix-Sums/README.md
@@ -0,0 +1,12 @@
+# Prefix Sums Pattern
+
+## Concept
+A Prefix Sum array stores the cumulative sum of elements from the start of the array. This allows for constant-time range sum queries.
+
+## When to Use
+- When you need to perform multiple sum queries over different ranges of an array.
+
+## Time & Space Complexity
+- **Pre-computation:** O(n)
+- **Query:** O(1)
+- **Space:** O(n)
---
+++ DSA/Arrays/Prefix-Sums/RangeSumQuery.java
@@ -0,0 +1,15 @@
+public class RangeSumQuery {
+    public static void main(String[] args) {
+        int[] nums = {5, 6, 7, 1};
+        int[] prefix = new int[nums.length];
+
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        // Sum from index 1 to 3
+        int sum = prefix[3] - prefix[0];
+        System.out.println("Sum of index 1 to 3 is: " + sum);
+    }
+}
